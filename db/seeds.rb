07e295b872c85f36c531a3477b855697fe0eeb7c
Manuel Velasco <velascome@gmail.com>
2019-07-12 18:41:00 +0200

    Rails mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "destroying previous seeds..."
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts "Creating ingredients"
+
+data = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+response = JSON.parse(data)
+
+drinks = response["drinks"]
+
+drinks.each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
+
+puts "Seed complete"
+
+Ingredient.all.each do |ingredient|
+  puts ingredient.name
+end
+
+Cocktail.create(name: "Bob")
+Cocktail.create(name: "Sara")
+Cocktail.create(name: "Bob1")
+Cocktail.create(name: "Sara1")
+Cocktail.create(name: "Bob2")
+Cocktail.create(name: "Sara2")
+Cocktail.create(name: "Bob3")
+Cocktail.create(name: "Sara3")
